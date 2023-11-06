c6eeb1707680383d7f4ec3135f3dfc0ac8d3b137
Norgh <pierre.senechal@student.junia.com>
2023-11-06 12:25:34 +0100

    feat(tentative de correction des bugs concernant author et ajout de fonctionnalités

--- library-api/src/useCases/authors/author.useCases.ts
+++ library-api/src/useCases/authors/author.useCases.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { AuthorId } from 'library-api/src/entities';
+import { AuthorRepository } from 'library-api/src/repositories';
+import {
+  CreateAuthorUseCasesInput,
+  PlainAuthorUseCasesOutput,
+} from 'library-api/src/useCases/authors/author.useCases.type';
 
 @Injectable()
-export class AuthorUseCases {}
+export class AuthorUseCases {
+  constructor(private readonly authorRepository: AuthorRepository) {}
+
+  /**
+   * Get all plain authors
+   * @returns Array of plain authors
+   */
+  public async getAllPlain(): Promise<PlainAuthorUseCasesOutput[]> {
+    return this.authorRepository.getAllPlain();
+  }
+
+  /**
+   * Get a author by its ID
+   * @param id Author's ID
+   * @returns Author if found
+   * @throws 404: author with this ID was not found
+   */
+  public async getById(id: AuthorId): Promise<PlainAuthorUseCasesOutput> {
+    return this.authorRepository.getById(id);
+  }
+
+  /**
+   * Create plain author
+   * @param id Author's ID
+   * @returns Author
+   * @throws 404: author with this ID was not found
+   */
+
+  public async createPlainAuthor(
+    input: CreateAuthorUseCasesInput,
+  ): Promise<PlainAuthorUseCasesOutput> {
+    return this.authorRepository.createAuthor(input);
+  }
+
+  public async deletePlainAuthor(id: AuthorId): Promise<void> {
+    const author = await this.getById(id);
+    await this.authorRepository.deleteAuthor(author.id);
+  }
+}
